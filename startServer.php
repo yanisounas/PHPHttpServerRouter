255a444f3649c8791c45f3c409f0d649185b02a7
yanisounas <106371569+yanisounas@users.noreply.github.com>
2022-11-19 16:38:59 +0100

    Update startServer.php

--- startServer.php
+++ startServer.php
@@ -4,7 +4,7 @@ chdir(__DIR__. "/public/");
 require "../vendor/autoload.php";
 
 if (PHP_VERSION < "8.0.0")
-    exit(throw new \Exception("PHP version mus be at least 8.0.0 Your version : ". PHP_VERSION));
+    exit(throw new \Exception("PHP version must be at least 8.0.0; Your version : ". PHP_VERSION));
 
 PHPHttpServerRouter\Router\RouterInitializer::getInstance(autoStart: true);
 
